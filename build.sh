b2e60b49c8b79ee3f2712b21a5b6801173e0b50e
Oscar Muñoz <osmunogue@gmail.com>
2023-09-26 00:15:56 -0300

    build was modified

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+pip install --upgrade pip
+pip install poetry
+/opt/render/project/src/.venv/bin/poetry install
 poetry install
 
 python manage.py collectstatic --no-input
