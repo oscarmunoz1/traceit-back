58b39c5073683d993ed1f0274e9bb795e4835f23
Oscar Muñoz <osmunogue@gmail.com>
2023-09-27 23:07:27 -0300

    build was modified

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 pip install --upgrade pip
 pip install poetry
 
+# Ensure the virtual environment is activated
+source /opt/render/project/src/.venv/bin/activate
+
 poetry env use 3.9
 
-# Add this line to ensure Python 3.9 is in the PATH
-export PATH=$HOME/.poetry/bin:$HOME/.poetry/envs/3.9/bin:$PATH
-
-/opt/render/project/src/.venv/bin/poetry install
+# Install dependencies using Poetry
 poetry install
 
+# Run your Django management commands
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
