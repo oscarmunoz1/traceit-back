15802d6898a07594fb6fad024348c29f132347ac
Oscar Muñoz <osmunogue@gmail.com>
2023-09-27 22:49:08 -0300

    build file was modified

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 pip install --upgrade pip
 pip install poetry
-poetry env use 3.9
+
 /opt/render/project/src/.venv/bin/poetry install
 poetry install
-
+poetry env use 3.9
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
