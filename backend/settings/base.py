b20c6be9c3eaf14806bfc6370c383e22c6f4846e
Oscar Muñoz <osmunogue@gmail.com>
2023-09-30 23:21:29 -0300

    Some changes were made

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -104,16 +104,37 @@ GEOIP_PATH = os.path.join(Path(__file__).resolve().parent.parent.parent, "geoip_
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("DATABASE_NAME"),
+#         "USER": config("DATABASE_USER"),
+#         "PASSWORD": config("DATABASE_PASSWORD"),
+#         "HOST": config("DATABASE_HOST"),
+#         "PORT": config("DATABASE_PORT"),
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DATABASE_NAME"),
-        "USER": config("DATABASE_USER"),
-        "PASSWORD": config("DATABASE_PASSWORD"),
-        "HOST": config("DATABASE_HOST"),
-        "PORT": config("DATABASE_PORT"),
+        "NAME": "traceitdb",
+        "USER": "postgres",
+        "PASSWORD": "PlazaFlores_",
+        "HOST": "traceit-database.cox3o9tj0v96.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
+
+AWS_ACCESS_KEY_ID = "AKIAQGLIFETF2HN67DMN "
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "traceit-bucket"
+AWS_S3_SIGNATURE_NAME = ("s3v4",)
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 # Password validation
