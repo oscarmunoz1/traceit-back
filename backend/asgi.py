2767e2d41cda2b066e7a908a48d6dbd47763caa1
Oscar Muñoz <osmunogue@gmail.com>
2023-10-01 22:06:53 -0300

    Changes in wsgi file were made

--- backend/asgi.py
+++ backend/asgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from decouple import config
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+if DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.prod")
 
 application = get_asgi_application()
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+from decouple import config
+from dotenv import load_dotenv
+from pathlib import Path
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+if DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.prod")
 
 application = get_wsgi_application()
