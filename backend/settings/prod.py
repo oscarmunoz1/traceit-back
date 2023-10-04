81803f2cf579c4242c8f2fb27d7591bd770eecb5
Oscar Muñoz <osmunogue@gmail.com>
2023-10-04 16:48:22 -0300

    settings file was modified

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,3 +1,21 @@
 import os
 
 from backend.settings.base import *
+
+
+ALLOWED_HOSTS = ["api-us-east-1.traceit.io"]
+
+
+CSRF_TRUSTED_ORIGINS = [".traceit.io", "traceit.io", "api-us-east-1.traceit.io"]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = ["https://app.traceit.io", "https://traceit.io"]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = ["https://app.traceit.io", "https://traceit.io"]
+
+CSRF_COOKIE_DOMAIN = "traceit.io"
+
+
+CSRF_COOKIE_SECURE = True
